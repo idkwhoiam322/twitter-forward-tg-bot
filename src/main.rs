18a83758a847ca4875f113e9502785c154a23584
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2021-07-29 13:22:12 +0530

    main: Change date/time to monospace and add bot name to start text
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ mod twitter;
 use twitter::manage_tweets::send_tweets;
 
 use teloxide::prelude::*;
+use teloxide::types::{
+    ParseMode, Me,
+};
 use chrono::prelude::*;
 
 async fn run() {
@@ -15,12 +18,16 @@ async fn run() {
 
     let chat_id:i64 = -1001527066155; // test chat
 
+    let Me { user: bot_user, .. } = tg_bot.get_me().send().await.unwrap();
+    let bot_name = bot_user.username.expect("Bots must have usernames");
     let utc_time: DateTime<Utc> = chrono::Utc::now();
-    let startpost_text = format!("Starting bot at {}-{}-{} {}:{}:{} UTC.",
-                utc_time.year(), utc_time.month(), utc_time.day(),
-                utc_time.hour(), utc_time.minute(), utc_time.second());
+    let startpost_text = format!("Starting @{} at <code>{}-{}-{} {}:{}:{} UTC</code>.",
+                            bot_name,
+                            utc_time.year(), utc_time.month(), utc_time.day(),
+                            utc_time.hour(), utc_time.minute(), utc_time.second());
 
     tg_bot.send_message(chat_id, startpost_text)
+        .parse_mode(ParseMode::Html)
         .send()
         .await
         .expect("Message could not be sent");
